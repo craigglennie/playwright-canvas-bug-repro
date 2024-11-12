560eddfce495c367cec659e7892b6499df838fa1
Craig Glennie <cglennie@aha.io>
2024-11-13 11:09:35 +1300

    Wait in test because something canvas doesn't show anything otherwise

--- tests/canvas-bug.spec.ts
+++ tests/canvas-bug.spec.ts
@@ -3,6 +3,8 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:1234/');
 
+  await page.waitForTimeout(1000);
+
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("Canvas with Overlay");
 });
